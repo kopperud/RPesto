cbc45c90d6c1cfcba9fd8365688439f716287d65
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-06-24 11:40:28 +0200

    make a dummy function that prints the phylogeny

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -19,10 +19,27 @@ fn hello_world() -> &'static str {
     "Hello world!"
 }
 
+/// @export
+#[extendr]
+fn print_me_a_tree(s: String) -> (){
+    let tree = parse_tree(s);
+
+    println!("{:?}", tree);
+}
+
+fn likelihood(lambda: f64, mu: f64, phy: String) -> f64{
+    let tree = parse_tree(phy);
+
+    let lnl = 0.0;
+
+    return lnl;
+}
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod RPesto;
     fn hello_world;
+    fn print_me_a_tree;
 }
