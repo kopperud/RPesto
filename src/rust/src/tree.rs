289d1401e33243d50e743ccddf470abb900fefe0
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-06-27 11:43:44 +0200

    try to index tree, not working

--- src/rust/src/tree.rs
+++ src/rust/src/tree.rs
@@ -15,6 +15,7 @@ use extendr_api::prelude::*;
 pub struct Node {
     pub label: String,
     pub length: f64,
+    //pub index: i32,
     pub children: Vec<Box<Node>>,
 }
 
@@ -23,9 +24,34 @@ pub struct Node {
 impl Node {
     pub fn new(label: String, length: f64) -> Self {
         let children: Vec<Box<Node>> = Vec::new(); 
-
+        //let index = 0;
+        //Self {label, length, index, children }
         Self {label, length, children }
     }
+
+    /*
+    fn reindex(&self) -> (){
+        let node_index: i32 = 1;
+        let tip_index: i32 = 1;
+        
+    }
+
+    fn reindex_postorder(&mut self, tip_index: &mut i32) -> (){
+        let n_children = self.children.len();
+        if n_children == 0{
+            self.index = tip_index.clone();
+            (*tip_index) += 1;
+        }else{
+           for child in self.children.iter(){
+               child.reindex_postorder(tip_index);
+           }
+        }
+    }
+
+    fn reindex_preorder(&self, node_index: &mut i32) -> (){
+    }
+    */
+
 }
 
 extendr_module! {
