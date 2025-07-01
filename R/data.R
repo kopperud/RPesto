82296c1ffe33599d3145587d75dfc8e430717669
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-07-01 14:18:57 +0200

    add primates dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Primates phylogenetic tree
+#' 
+#' This tree is from Vos & Mooers (2006)
+#'
+#' Vos, R. and A. Mooers. 2006. A new dated supertree of the primates. in Inferring large phylogenies: the big tree problem (R Vos, Phd thesis). Simon Fraser University, Burnaby, British Columbia.
+#' 
+#' @docType data
+#' 
+#' @usage data(primates)
+#' 
+"primates"
