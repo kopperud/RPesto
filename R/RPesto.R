d4e4783ae4648d73f4926bb80546fb641f50b943
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-07-04 11:27:12 +0200

    better metadata

---
+++ R/RPesto.R
@@ -0,0 +1,13 @@
+#'RPesto: Phylogenetic Estimation of Shifts in the Tempo of Origination (in R)
+#'
+#'
+#'
+#'
+#'
+#' @section References:
+#' \itemize{
+#'  \item Kopperud, B. T., & Höhna, S. (2025). Phylogenetic Estimation of branch-specific Shifts in the Tempo of Origination. Systematic Biology, syaf041.
+#' 
+#'  \item Höhna, S., Freyman, W. A., Nolen, Z., Huelsenbeck, J. P., May, M. R., & Moore, B. R. (2019). A Bayesian approach for estimating branch-specific speciation and extinction rates. BioRxiv, 555805.
+#' }
+"_PACKAGE"
