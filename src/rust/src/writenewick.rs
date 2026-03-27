d2ac3b0d2abe9b6e3097400924d42aeb0a60075b
kopperud <kopperud@protonmail.com>
2026-03-27 10:25:52 +0100

    white space

--- src/rust/src/writenewick.rs
+++ src/rust/src/writenewick.rs
@@ -1,12 +1,12 @@
 use crate::tree::*;
 
-pub trait WriteNewick{
-    fn writenewick( &self ) -> String;
-    fn writenewick_preorder( &self, s: &mut String) -> ();
+pub trait WriteNewick {
+    fn writenewick(&self) -> String;
+    fn writenewick_preorder(&self, s: &mut String) -> ();
 }
 
-impl WriteNewick for Node{
-    fn writenewick( &self ) -> String{
+impl WriteNewick for Node {
+    fn writenewick(&self) -> String {
         let mut s = "".to_string();
 
         self.writenewick_preorder(&mut s);
@@ -16,19 +16,19 @@ impl WriteNewick for Node{
         return s;
     }
 
-    fn writenewick_preorder( &self, s: &mut String) -> () {
+    fn writenewick_preorder(&self, s: &mut String) -> () {
         let n_children = self.children.len();
 
-        let is_tip = self.children.is_empty();        
+        let is_tip = self.children.is_empty();
 
-        if is_tip{
+        if is_tip {
             s.push_str(self.label.as_str());
-        }else{
+        } else {
             s.push('(');
-            for (i, child_node) in self.children.iter().enumerate(){
+            for (i, child_node) in self.children.iter().enumerate() {
                 child_node.writenewick_preorder(s);
 
-                if i < (n_children-1){
+                if i < (n_children - 1) {
                     s.push(',');
                 }
             }
@@ -47,24 +47,21 @@ impl WriteNewick for Node{
         add_variable(self.bayes_factor, &mut items, "shift_bf");
         //add_variable(self.tip_netdiv, &mut items, "tip_netdiv");
 
-        if !items.is_empty(){
+        if !items.is_empty() {
             let joined = items.join(",");
             let x = format!("[&{}]", joined);
             s.push_str(x.as_str());
         }
-        
+
         s.push(':');
         let brlen = self.length.to_string();
         s.push_str(brlen.as_str());
     }
 }
 
-
-fn add_variable(variable: Option<f64>, items: &mut Vec<String>, label: &str) -> (){
-    match variable{
-       Some(x) => items.push(format!("{}={}", label, x)),
-       _ => (),
+fn add_variable(variable: Option<f64>, items: &mut Vec<String>, label: &str) -> () {
+    match variable {
+        Some(x) => items.push(format!("{}={}", label, x)),
+        _ => (),
     }
 }
-
-
